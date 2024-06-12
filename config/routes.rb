d4b21aaba5e94dd337190fa884e42a50012da091
giorgos diamantidis <gdiamantidisd@gmail.com>
2024-06-11 22:02:19 -0400

    ejercicio 2 avanzado

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "tasks", to: "tasks#index"
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new", as: :new_task   #http://127.0.0.1:3000/tasks/new
   get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks/:id", to: "tasks#show", as: :task
+  patch "tasks/:id", to: "tasks#update"
+
+
 
   # Defines the root path route ("/")
   # root "posts#index"
